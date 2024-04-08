fcf92a743775be531556ca3bb7682a61b670c85f
Home <home@Homes-MacBook-Air.local>
2024-04-08 20:30:24 +0530

    minor fix

--- my-app/components/Chessboard.js
+++
@@ -1,9 +0,0 @@
-// components/Chessboard.js
-import React from "react";
-import { Chessboard } from "react-chessboard";
-
-const ChessboardComponent = ({ position, onPieceDrop }) => {
-  return <Chessboard position={position} onPieceDrop={onPieceDrop} />;
-};
-
-export default ChessboardComponent;
